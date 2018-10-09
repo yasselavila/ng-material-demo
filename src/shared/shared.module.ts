d6ffc9c77ae6f7a3435a8a561a1e010a5ce64b44
Yassel Avila <yassel.avila@gmail.com>
2018-10-09 14:38:08 -0400

    Layout

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -2,14 +2,27 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { SHARED_COMPONENTS } from './components';
 import { MATERIAL_MODULES } from './material-modules';
 
 /* Do not specify providers for modules that might be imported by a lazy loaded module */
 
 @NgModule({
-  imports: [CommonModule, RouterModule, HttpClientModule, ...MATERIAL_MODULES],
-  exports: [CommonModule, RouterModule, ...MATERIAL_MODULES, ...SHARED_COMPONENTS],
+  imports: [
+    CommonModule,
+    RouterModule,
+    HttpClientModule,
+    PerfectScrollbarModule,
+    ...MATERIAL_MODULES
+  ],
+  exports: [
+    CommonModule,
+    RouterModule,
+    PerfectScrollbarModule,
+    ...MATERIAL_MODULES,
+    ...SHARED_COMPONENTS
+  ],
   declarations: [...SHARED_COMPONENTS],
   entryComponents: [...SHARED_COMPONENTS]
 })
